fc6b0e59e77c07cebb30ba25d2a6cd6b0a4c0430
colin-chadwick <62803839+colin-chadwick@users.noreply.github.com>
2022-09-06 16:42:14 +0200

    1 binaries uploaded

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
+  title: 'Help Center',
   tagline: 'Dinosaurs are cool',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
@@ -50,7 +50,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'My Site',
+        title: 'Help Center',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
