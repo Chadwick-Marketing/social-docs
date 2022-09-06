9f84b07436712d4bec4ccf176be68359cfe3e16e
colin-chadwick <62803839+colin-chadwick@users.noreply.github.com>
2022-09-06 17:22:02 +0200

    1 binaries uploaded

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,6 @@ const config = {
   title: 'Help Center',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
-  onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
 
